a7adc29a08785960592ceab03e49f5e8449cca01
André De Bem <andrebemferraz@gmail.com>
2021-06-09 15:07:41 -0300

    Request 4 and 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { emailUser } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">
+          User:&nbsp;
+          {/* O código acima é para adicionar um espaço "forçado"
+          entre a primeira linha e a segunda */}
+          { emailUser }
+        </span>
+        <span data-testid="total-field">
+          Total expenses:&nbsp;
+          { 0 }
+        </span>
+        <span data-testid="header-currency-field">
+          BRL
+        </span>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  emailUser: state.user.email,
+});
+
+Wallet.propTypes = {
+  emailUser: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
